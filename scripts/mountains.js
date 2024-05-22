e77756077e6e2ca9cf31b4f7f61d069c7f53c731
Lesly Lopez <leslylpz32@gmail.com>
2024-05-22 13:46:29 -0500

    added mountains card with images and description

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,16 +1,137 @@
 "use strict"
 
 
-window.onload = () => {
+//window.onload = () => {
 
-    /*
-    You can remove the following console.log() lines.
-    They are here to verify that we have access to the data
-    The data script files are located in the scripts/data directory
+/*
+You can remove the following console.log() lines.
+They are here to verify that we have access to the data
+The data script files are located in the scripts/data directory
 */
 
-    //log the mountainsArray to the console (scripts/data/mountainData.js)
-    console.log(mountainsArray)
+//log the mountainsArray to the console (scripts/data/mountainData.js)
+//console.log(mountainsArray)
 
+
+//}
+
+window.onload = () => {
+
+    initMountainsDropdown();
+
+    let mountainDropdown = document.querySelector("#mountainSelect");
+
+    //display selected mountain when dropdown is changed
+    mountainDropdown.addEventListener("change", displayMountainCard)
 
 }
+
+function displayMountainCard(event) {
+
+    //the div where the mountain will be displayed
+    let mountainDiv = document.querySelector("#mountains")
+
+    //clears out previous mountain
+    mountainDiv.innerHTML = "";
+
+    //get selected value from dropdown
+    let selectedMountain = event.target.value;
+
+    //find matching mountain in the mountains array
+    let mountain = mountainsArray.find((mountain) => {
+
+        if (mountain.name === selectedMountain) {
+
+            return true;
+
+        }
+
+        return false;
+
+    })
+
+    /*
+    bootstrap card example:
+    
+    <div class="card">
+      <img src="PET IMAGE PATH GOES HERE" class="card-img-top" alt="ALT TEXT HERE">
+      <div class="card-body">
+        <h5 class="card-title">PET NAME HERE</h5>
+        <p class="card-text">PET DETAILS HERE</p>
+      </div>
+    </div>
+    */
+
+//     <ul class="list-group list-group-flush">
+//     <li class="list-group-item">An item</li>
+//     <li class="list-group-item">A second item</li>
+//     <li class="list-group-item">A third item</li>
+//   </ul>
+
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "w-25");
+
+    // let cardTitle = document.createElement("h5");
+    // cardTitle.classList.add("card-title");
+
+    // cardTitle.innerText = mountain.name;
+
+    // cardBody.appendChild(cardTitle);
+
+    let cardImage = document.createElement("img")
+    cardImage.classList.add("card-img-top");
+
+    cardImage.src = mountain.img;
+    cardImage.alt = mountain.name;
+
+    cardDiv.appendChild(cardImage)
+
+    let cardBody = document.createElement("div");
+    cardDiv.classList.add("card-body");
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-sub-title");
+
+    cardTitle.innerText = mountain.name;
+
+    cardBody.appendChild(cardTitle);
+
+    let cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+
+    cardText.innerHTML =
+        `${mountain.desc}`
+
+    cardBody.appendChild(cardText);
+
+    cardDiv.appendChild(cardBody);
+
+    mountainDiv.append(cardDiv);
+
+};
+
+function initMountainsDropdown() {
+
+    let mountainDropdown = document.querySelector("#mountainSelect");
+
+    let defaultOption = document.createElement("option");
+
+    defaultOption.value = "";
+
+    defaultOption.textContent = "Select a Mountain";
+
+    mountainDropdown.appendChild(defaultOption);
+
+    mountainsArray.forEach((mountain) => {
+
+        let newOption = document.createElement("option");
+
+        newOption.value = mountain.name;
+
+        newOption.textContent = mountain.name;
+
+        mountainDropdown.appendChild(newOption);
+
+    })
+
+};
